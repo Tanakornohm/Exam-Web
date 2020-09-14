2bcc4e36e4e45a8f859b9b9d4caf8fd45cdc1a3b
Tanakorn <tanakorn.panthaikul@gmail.com>
2020-09-14 18:07:33 +0700

    updated real time face detect + recog

--- test-face-recog/face-recognition-web/src/script.js
+++ test-face-recog/face-recognition-web/src/script.js
@@ -1,39 +1,44 @@
-const imageUpload = document.getElementById('imageUpload')
+const video = document.getElementById('video')
 
 Promise.all([
     faceapi.nets.faceRecognitionNet.loadFromUri('../models'),
     faceapi.nets.faceLandmark68Net.loadFromUri('../models'),
-    faceapi.nets.ssdMobilenetv1.loadFromUri('../models')
-]).then(start)
+    faceapi.nets.ssdMobilenetv1.loadFromUri('../models'),
+    faceapi.nets.tinyFaceDetector.loadFromUri('../models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('../models')
+]).then(startVideo)
 
-async function start() {
+function startVideo() {
+    navigator.getUserMedia(
+        { video: {} },
+        stream => video.srcObject = stream,
+        err => console.error(err)
+    )
+}
+video.addEventListener('play',async () => {
     const container = document.createElement('div')
     container.style.position = 'relative'
     document.body.append(container)
     const labeledFaceDescriptors = await loadLabeledImages()
+    document.body.append("Loaded")
     const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.55)
-    let image
-    let canvas
-    document.body.append('Loaded')
-    imageUpload.addEventListener('change', async () => {
-        if (image) image.remove()
-        if (canvas) canvas.remove()
-        image = await faceapi.bufferToImage(imageUpload.files[0])
-        container.append(image)
-        canvas = faceapi.createCanvasFromMedia(image)
-        container.append(canvas)
-        const displaySize = { width: image.width, height: image.height }
-        faceapi.matchDimensions(canvas, displaySize)
-        const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
+    
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions().withFaceDescriptors()
         const resizedDetections = faceapi.resizeResults(detections, displaySize)
         const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
         results.forEach((result, i) => {
             const box = resizedDetections[i].detection.box
             const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
             drawBox.draw(canvas)
         })
-    })
-}
+    }, 100)
+})
 
 function loadLabeledImages() {
     const labels = ['Narongded', 'Dorn', 'Tanakorn'];
